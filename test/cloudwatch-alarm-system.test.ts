2e84de3c881ff268c6b89ccc1990171b5ecacee5
Daniel Her <danieljhher@gmail.com>
2024-12-07 15:29:38 -0800

    initialized cdk

---
+++ test/cloudwatch-alarm-system.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudwatchAlarmSystem from '../lib/cloudwatch-alarm-system-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloudwatch-alarm-system-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudwatchAlarmSystem.CloudwatchAlarmSystemStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
